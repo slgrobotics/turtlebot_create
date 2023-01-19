3e8336a6529d56f3bbc627f23bfdeaf8ab1c2f8f
Sergei Grichine <msghub@hotmail.com>
2023-01-18 18:56:55 -0600

    Update bootup_launch.sh

--- RPi_Setup/launch/bootup_launch.sh
+++ RPi_Setup/launch/bootup_launch.sh
@@ -4,5 +4,6 @@ cd /home/ros/launch
 source /opt/ros/humble/setup.bash
 source /home/ros/create_ws/install/setup.bash
 source /home/ros/xv_11_ws/install/setup.bash
+source /home/ros/bno055_ws/install/setup.bash
 
 ros2 launch /home/ros/launch/myturtle.py
