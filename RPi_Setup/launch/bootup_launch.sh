007da43c9b2b7edb057dec01a6f26a256c1958be
Sergei Grichine <msghub@hotmail.com>
2023-02-09 19:11:09 -0600

    Update bootup_launch.sh

--- RPi_Setup/launch/bootup_launch.sh
+++ RPi_Setup/launch/bootup_launch.sh
@@ -2,7 +2,7 @@
 
 cd /home/ros/launch
 source /opt/ros/humble/setup.bash
-source /home/ros/create_ws/install/setup.bash
+source /home/ros/create_robot_ws/install/setup.bash
 source /home/ros/xv_11_ws/install/setup.bash
 source /home/ros/bno055_ws/install/setup.bash
 
