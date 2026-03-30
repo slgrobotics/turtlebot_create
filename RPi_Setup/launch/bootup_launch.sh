61c5bf3ec5906facd2277e9abf3e38add9bb63a4
Sergei Grichine <msghub@hotmail.com>
2026-03-29 19:48:59 -0500

    Update ROS setup script to use 'jazzy' version

--- RPi_Setup/launch/bootup_launch.sh
+++ RPi_Setup/launch/bootup_launch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd /home/ros/launch
-source /opt/ros/humble/setup.bash
+source /opt/ros/jazzy/setup.bash
 source /home/ros/create_robot_ws/install/setup.bash
 source /home/ros/xv_11_ws/install/setup.bash
 source /home/ros/bno055_ws/install/setup.bash
